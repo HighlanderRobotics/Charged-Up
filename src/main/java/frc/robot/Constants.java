793853d731d1fd98c86261cdd6c7a8cfd589e8b6
Lewis-Seiden <aPotatoeWithANaginata@gmail.com>
2023-02-04 11:58:34 -0800

    changed wheelbase and trackwidth for new dt

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,8 +43,8 @@ public final class Constants {
         COTSFalconSwerveConstants.SDSMK4i(COTSFalconSwerveConstants.driveGearRatios.SDSMK4i_L2);
 
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(21.73); //TODO: This must be tuned to specific robot
-    public static final double wheelBase = Units.inchesToMeters(21.73); //TODO: This must be tuned to specific robot
+    public static final double trackWidth = Units.inchesToMeters(22.7);
+    public static final double wheelBase = Units.inchesToMeters(24.5);
     public static final double wheelCircumference = chosenModule.wheelCircumference;
 
     /* Swerve Kinematics 
