183a23d5195ed2c73930f3a872d5ebb23d5bb957
Lewis-Seiden <aPotatoeWithANaginata@gmail.com>
2023-03-07 00:10:14 -0800

    updated some constants to our constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -417,32 +417,21 @@ public final class Constants {
     public static final List<VisionSource> VISION_SOURCES =
         List.of(
             new VisionSource(
-                "frontCam",
+                "limelight-right",
                 new Transform3d(
-                    // 9.867 in to the right looking from behind the front of the robot
-                    // 7 inch forward from center
-                    // up 17.422 inches
-                    new Translation3d(
-                        0.1778, // front/back
-                        0.2506218, // left/right
-                        0.4425188 // up/down
-                        ),
-                    new Rotation3d(
-                        0,
-                        Math.toRadians(-11.5), // angle up/down
-                        0))),
+                  new Translation3d(
+                    Units.inchesToMeters(-8),
+                    Units.inchesToMeters(-9.75), 
+                    Units.inchesToMeters(-22.75)),
+                  new Rotation3d(0, 0, Units.degreesToRadians(-5)))),
             new VisionSource(
-                "backCam",
+                "limelight-left",
                 new Transform3d(
-                    // 9.867 in to the right looking from behind the front of the robot
-                    // 48.5 inches up
-                    // two inches forward
-                    new Translation3d(
-                        0.0508, // front/back
-                        -0.2506218, // left/right
-                        1.2319 // up/down
-                        ),
-                    new Rotation3d(0, Math.toRadians(17), Math.PI))));
+                  new Translation3d(
+                    Units.inchesToMeters(-8),
+                    Units.inchesToMeters(9.75), 
+                    Units.inchesToMeters(-22.75)),
+                  new Rotation3d(0, 0, Units.degreesToRadians(5)))));
   }
 
   /** For 5026 vision code */
