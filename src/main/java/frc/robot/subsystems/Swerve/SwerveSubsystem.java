ac8520f09d2db863f72f06d5ec730b3c62916a3c
team computer <lewy@seiden.us>
2023-09-13 16:20:40 -0700

    commented out vision for drive practice

--- src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/Swerve/SwerveSubsystem.java
@@ -78,9 +78,9 @@ public class SwerveSubsystem extends SubsystemBase {
   private boolean isInTapeMode = true;
   private PIDController tapeDriveAssistController = new PIDController(-0.018, 0, -0.01);
 
-  private VisionIOApriltags apriltagVisionSubsystem = new VisionIOApriltags();
-  private VisionIOSimApriltags apriltagVisionSim = new VisionIOSimApriltags();
-  private VisionIOInputs visionIOInputs = new VisionIOInputs();
+  // private VisionIOApriltags apriltagVisionSubsystem = new VisionIOApriltags();
+  // private VisionIOSimApriltags apriltagVisionSim = new VisionIOSimApriltags();
+  // private VisionIOInputs visionIOInputs = new VisionIOInputs();
   private LinearFilter tapeYawFilter = LinearFilter.singlePoleIIR(0.2, 0.020);
   private double tapeYawFilterVal = 0;
 
@@ -104,8 +104,8 @@ public class SwerveSubsystem extends SubsystemBase {
   public ProfiledPIDController headingController =
       new ProfiledPIDController(1.2, 0, 0.1, new Constraints(Math.PI * 4, Math.PI * 6));
 
-  public VisionIOTape tapeVisionSubsystem =
-      new VisionIOTape("limelight-left", Constants.leftCameraToRobot);
+  // public VisionIOTape tapeVisionSubsystem =
+  //     new VisionIOTape("limelight-left", Constants.leftCameraToRobot);
 
   public SwerveSubsystem() {
     gyroIO = Robot.isReal() ? new GyroIOPigeon() : new GyroIOSim();
@@ -561,8 +561,8 @@ public class SwerveSubsystem extends SubsystemBase {
         .andThen(
             headingLockDriveCommand(
                     xSupplier,
-                    () ->
-                        tapeVisionSubsystem.hasTargets()
+                    () ->false
+                        // tapeVisionSubsystem.hasTargets()
                             ? tapeDriveAssistController.calculate(
                                 tapeYawFilterVal,
                                 Constants.Vision.simpleVisionSnapTarget + ySupplier.getAsDouble())
@@ -616,19 +616,19 @@ public class SwerveSubsystem extends SubsystemBase {
       wheelOnlyOdo.update(Rotation2d.fromDegrees(simHeading), getModulePositions());
     }
 
-    apriltagVisionSim.updateInputs(visionIOInputs, new Pose3d(pose));
-    Logger.getInstance().processInputs("Vision Sim IO", visionIOInputs);
+    // apriltagVisionSim.updateInputs(visionIOInputs, new Pose3d(pose));
+    // Logger.getInstance().processInputs("Vision Sim IO", visionIOInputs);
 
-    List<frc.robot.subsystems.Vision.VisionIO.VisionMeasurement> visionMeasurements =
-        apriltagVisionSubsystem.getMeasurement(pose);
+    // List<frc.robot.subsystems.Vision.VisionIO.VisionMeasurement> visionMeasurements =
+    //     apriltagVisionSubsystem.getMeasurement(pose);
 
-    for (frc.robot.subsystems.Vision.VisionIO.VisionMeasurement measurement : visionMeasurements) {
-      dashboardFieldVisionPoses.add(measurement.estimation.estimatedPose.toPose2d());
-      poseEstimator.addVisionMeasurement(
-          measurement.estimation.estimatedPose.toPose2d(),
-          measurement.estimation.timestampSeconds,
-          measurement.confidence.times(2.0));
-    }
+    // for (frc.robot.subsystems.Vision.VisionIO.VisionMeasurement measurement : visionMeasurements) {
+    //   dashboardFieldVisionPoses.add(measurement.estimation.estimatedPose.toPose2d());
+    //   poseEstimator.addVisionMeasurement(
+    //       measurement.estimation.estimatedPose.toPose2d(),
+    //       measurement.estimation.timestampSeconds,
+    //       measurement.confidence.times(2.0));
+    // }
 
     field.setRobotPose(getPose());
     field.getObject("odo only pose").setPose(wheelOnlyOdo.getPoseMeters());
@@ -647,6 +647,6 @@ public class SwerveSubsystem extends SubsystemBase {
     double filteredRoll = rollFilter.calculate(gyroIO.getRollDegrees());
     rollRate = (filteredRoll - lastRoll) / 0.020;
     lastRoll = filteredRoll;
-    tapeYawFilterVal = tapeYawFilter.calculate(tapeVisionSubsystem.getYaw());
+    // tapeYawFilterVal = tapeYawFilter.calculate(tapeVisionSubsystem.getYaw());
   }
 }
